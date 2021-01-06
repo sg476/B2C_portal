e0bc75a726388d727d1157c7a033e2d290d79961
sg476 <sid.gupta476@gmail.com>
2021-01-06 12:47:09 +0530

    EnrolmentEdit | Siddharth Gupta

--- src/component/EnrollmentForm.js
+++ src/component/EnrollmentForm.js
@@ -20,189 +20,194 @@ export class Enrollment extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className=" container-fluid offset-2 col-md-8 ">
-          <h2 className="heading">Enrollment - New User Registration </h2>
-          <div className="card form shadow p-3 mb-5 rounded">
-            <form>
-              <h4 className="text-center">Choose appropriate options</h4>
-              <table>
-                <tbody>
-                  <tr>
-                    <td colSpan="2">
-                      <span className="col_1">
-                        Applicant Type<span className="text-danger">*</span>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="offset-2 col-md-8 ">
+              <h2 className="heading mb-5 mt-5">Enrollment - New User Registration </h2>
+              <div className="card shadow p-3 mb-5 rounded">
+                <form>
+                  <h4 className="text-center">Choose appropriate options</h4>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td colSpan="2">
+                          <span className="col_1">
+                            Applicant Type<span className="text-danger">*</span>
                         &nbsp;
                       </span>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="applicant"
-                          id="Individual"
-                          value="Individual"
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="Individual"
-                        >
-                          Individual Subscriber
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="applicant"
+                              id="Individual"
+                              value="Individual"
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="Individual"
+                            >
+                              Individual Subscriber
                         </label>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="applicant"
-                          id="Corporate"
-                          value="Corporate"
-                        />
-                        <label className="form-check-label" htmlFor="Corporate">
-                          Corporate Subscriber
+                          </div>
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="applicant"
+                              id="Corporate"
+                              value="Corporate"
+                            />
+                            <label className="form-check-label" htmlFor="Corporate">
+                              Corporate Subscriber
                         </label>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2">
-                      <div className="col_1">
-                        Status of Application
+                          </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colSpan="2">
+                          <div className="col_1">
+                            Status of Application
                         <span className="text-danger">*</span>&nbsp;
                       </div>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="SOP"
-                          id="ci"
-                          value="ci"
-                        />
-                        <label className="form-check-label" htmlFor="ci">
-                          Citizen of India
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="SOP"
+                              id="ci"
+                              value="ci"
+                            />
+                            <label className="form-check-label" htmlFor="ci">
+                              Citizen of India
                         </label>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="SOP"
-                          id="nri"
-                          value="nri"
-                        />
-                        <label className="form-check-label" htmlFor="nri">
-                          Non Resident of India
+                          </div>
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="SOP"
+                              id="nri"
+                              value="nri"
+                            />
+                            <label className="form-check-label" htmlFor="nri">
+                              Non Resident of India
                         </label>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="SOP"
-                          id="oci"
-                          value="oci"
-                        />
-                        <label className="form-check-label" htmlFor="oci">
-                          Overseas Citizen of India
+                          </div>
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="SOP"
+                              id="oci"
+                              value="oci"
+                            />
+                            <label className="form-check-label" htmlFor="oci">
+                              Overseas Citizen of India
                         </label>
-                      </div>
-                    </td>
-                  </tr>
-                  <br />
-                  <tr>
-                    <td colSpan="2">
-                      <div className="col_1">
-                        Register With<span className="text-danger">*</span>
+                          </div>
+                        </td>
+                      </tr>
+                      <br />
+                      <tr>
+                        <td colSpan="2">
+                          <div className="col_1">
+                            Register With<span className="text-danger">*</span>
                         &nbsp;
                       </div>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="register"
-                          id="aadhaar"
-                          value="aadhaar"
-                        />
-                        <label className="form-check-label" htmlFor="aadhaar">
-                          Aadhaar offline e-KYC
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="register"
+                              id="aadhaar"
+                              value="aadhaar"
+                            />
+                            <label className="form-check-label" htmlFor="aadhaar">
+                              Aadhaar offline e-KYC
                         </label>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="register"
-                          id="pan"
-                          value="pan"
-                        />
-                        <label className="form-check-label" htmlFor="pan">
-                          Permanent Account Number(Pan)
+                          </div>
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="register"
+                              id="pan"
+                              value="pan"
+                            />
+                            <label className="form-check-label" htmlFor="pan">
+                              Permanent Account Number(Pan)
                         </label>
-                      </div>
-                    </td>
-                  </tr>
-                  <br />
-                  <tr>
-                    <td colSpan="2">
-                      <span className="col_1">
-                        Account Type<span className="text-danger">*</span>&nbsp;
+                          </div>
+                        </td>
+                      </tr>
+                      <br />
+                      <tr>
+                        <td colSpan="2">
+                          <span className="col_1">
+                            Account Type<span className="text-danger">*</span>&nbsp;
                       </span>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="accType"
-                          id="tier_12"
-                          value="tier_12"
-                        />
-                        <label className="form-check-label" htmlFor="tier_12">
-                          Tier I &amp;&amp; Tier II
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="accType"
+                              id="tier_12"
+                              value="tier_12"
+                            />
+                            <label className="form-check-label" htmlFor="tier_12">
+                              Tier I &amp;&amp; Tier II
                         </label>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="form-check form-check-inline">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="accType"
-                          id="tier_1"
-                          value="tier_1"
-                        />
-                        <label className="form-check-label" htmlFor="tier_1">
-                          Tier I
+                          </div>
+                        </td>
+                        <td>
+                          <div className="form-check form-check-inline">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="accType"
+                              id="tier_1"
+                              value="tier_1"
+                            />
+                            <label className="form-check-label" htmlFor="tier_1">
+                              Tier I
                         </label>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="text-center btn">
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-block"
-                  onClick={this.handleLogin}
-                >
-                  Enroll
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="text-center btn">
+                    <button
+                      type="submit"
+                      className="btn btn-primary btn-block"
+                      onClick={this.handleLogin}
+                    >
+                      Enroll
                 </button>
+                  </div>
+                </form>
               </div>
-            </form>
+            </div>
           </div>
         </div>
+
       </React.Fragment>
     );
   }
